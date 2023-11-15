2179d479171d92c512fb09228dfc5d33ee09b929
embirtell <129796173+embirtell@users.noreply.github.com>
2023-11-15 13:49:04 -0500

    Removed the valve class, instead integrated it with the sensor class
    Successfully reading from sensors and opening valves

--- sensors-to-valves-classes.ino/sensors-to-valves-classes.ino.ino
+++ sensors-to-valves-classes.ino/sensors-to-valves-classes.ino.ino
@@ -2,26 +2,56 @@
 #include "Adafruit_seesaw.h"
 #include <Wire.h>
 
+//class representing valves
+class Valve {
+private:
+  int relayPin; //Arduino GPIO pin connected to the relay
+
+public:
+  Valve(int pin) : relayPin(pin) {
+    pinMode(relayPin, OUTPUT); // Set the relay pin as an output
+    close(); // Ensure valves are closed initially
+    Serial.print("Valve initialized on pin ");
+    Serial.println(relayPin);
+  }
+
+  void open() {
+    digitalWrite(relayPin, HIGH); // Activate the relay to open the valve
+    Serial.print("Valve on pin ");
+    Serial.print(relayPin);
+    Serial.println(" is OPEN");
+  }
+
+  void close(){
+    digitalWrite(relayPin, LOW); // Activate the relay to close the valve
+    Serial.print("Valve on pin ");
+    Serial.print(relayPin);
+    Serial.println(" is CLOSED");
+  }
+};
 
 
-
-
-
-
-
-
-
+//class representing soil sensors
 class SoilSensor {
 private:
   Adafruit_seesaw sensor;
-  int sensorIndex; //sensor index
+  Valve& associatedValve; //Each sensor is associated with a valve
+  uint16_t moistureThreshold; //Moisture level threshold for the sensor
+  int sensorIndex; //sensor index/identifier of sensor
+
 public:
   //constructor
-  SoilSensor(int index) : sensorIndex(index) {}
+  SoilSensor(Valve& valve, uint16_t threshold, int index) : associatedValve(valve), moistureThreshold(threshold), sensorIndex(index){}
+
+  ///SoilSensor(int index) : sensorIndex(index) {}
+  ///SoilSensor(Valve& valve) : associatedValve(valve) {}
 
   //Method to initialize the sensor with a specific I2C address
   void begin(uint8_t i2cAddress) {
     sensor.begin(i2cAddress);
+    Serial.print("Sensor ");
+    Serial.print(sensorIndex + 1);
+    Serial.println(" initialized");
   }
   //Method to get temp from sensor
   float getTemperature() {
@@ -31,30 +61,47 @@ public:
   uint16_t getCapacitiveReading(int pin) {
     return sensor.touchRead(pin);
   }
+  //Method to check soil moisture level and control valve
+  void checkMoistureLevel(){
+    uint16_t capRead = getCapacitiveReading(0); // Read capacitive value from the sensor
+
+    Serial.print("Reading from Sensor ");
+    Serial.print(sensorIndex + 1);
+    Serial.print("Capacitive Reading: ");
+    Serial.println(capRead);
+
+  //Open the valve if the capacitive reading is below threshold
+  if(capRead < moistureThreshold) {
+    Serial.println("Moisture level is low. Opening the valve.");
+    associatedValve.open();
+  } else {
+    Serial.println("Moisture level is sufficient. Closing the valve.");
+    associatedValve.close();
+  }
+}
   //Method to read and print data from the sensor
   void readData() {
+     //Get temp and capacitive reading from sensor
+    float tempC = getTemperature();
+    uint16_t capRead = getCapacitiveReading(0);
+    //Print the obtained data    
     Serial.print("Reading from Sensor ");
     Serial.println(sensorIndex + 1);
   
-    //Get temp and capacitive reading from sensor
-    float tempC = getTemperature();
-    uint16_t capRead = getCapacitiveReading(0);
-
-    //Print the obtained data
     Serial.print("Temperature: ");
     Serial.println(tempC);
+  
     Serial.print("Capacitive Reading: ");
     Serial.println(capRead);
     Serial.println();
-    }
+  }
 };
 
-//Number of soil moisture sensors
-const int numSensors = 3;
-
+const int numSensors = 3; //number of soil moisture sensors
+Valve valves[numSensors] = {Valve(2), Valve(3), Valve(4)}; //declare valves
 //Array of SoilSensor instances representing each sensor
+SoilSensor sensors[numSensors] = {SoilSensor(valves[0], 300, 0), SoilSensor(valves[1], 300, 1), SoilSensor(valves[2], 300, 2)};
 
-SoilSensor sensors[numSensors] = {SoilSensor(0), SoilSensor(1), SoilSensor(2)};
 
 void setup() {
   Serial.begin(115200); //Initialize serial communication
@@ -67,11 +114,16 @@ void setup() {
 }
 
 void loop() {
-   for (int i = 0; i < numSensors; ++i) {
-    sensors[i].readData();
+   //Read and report sensor data
+  for (int i = 0; i < numSensors; ++i) {
+    sensors[i].readData(); // Read and print additional data from the sensor
    }
   // Add a delay here if needed to control how often you read data from the sensors
   delay(1000);
-
-   }
+  
+  for(int i = 0; i < numSensors; ++i){
+    sensors[i].checkMoistureLevel(); // Check soil moisture level and control the valve accordingly
+  }
+  delay(1000);
+}
 
