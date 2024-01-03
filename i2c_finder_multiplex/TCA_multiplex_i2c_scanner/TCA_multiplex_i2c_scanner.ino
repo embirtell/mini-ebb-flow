e2e2de5ccdd658a26384fd42415e9e3e2d569a2c
skyefi <nickyreigel@gmail.com>
2024-01-03 12:26:05 -0500

    i2c Searchers Fixed and Organized
    Two i2c search programs, one for use with the breakout/multiplexor board, one that ignores any multiplexors

--- i2c_finder_multiplex/TCA_multiplex_i2c_scanner/TCA_multiplex_i2c_scanner.ino
+++ i2c_finder_multiplex/TCA_multiplex_i2c_scanner/TCA_multiplex_i2c_scanner.ino
@@ -4,11 +4,14 @@
  * Based on https://playground.arduino.cc/Main/I2cScanner/
  *
  */
+ //Modified by Eva and Nicky, Sparks Lab
+ //1-3-2024
 
 #include "Wire.h"
 
 #define TCA9548A 0x70
 
+//Tells the breakout board (TCA chip) to open one port at a time
 void tcaselect(uint8_t i) {
   if (i > 7) return;
  
@@ -18,26 +21,26 @@ void tcaselect(uint8_t i) {
 }
 
 
-// standard Arduino setup()
 void setup()
 {
-    while (!Serial);
     delay(1000);
     
-
-    Wire.begin();
+    Wire.begin();   //Start i2c interface
     
     Serial.begin(115200);
     Serial.println("\nTCAScanner ready!");
     
+    //Outer for loop - select one i2c breakout port at a time
     for (uint8_t t=0; t<8; t++) {
       tcaselect(t);
       Serial.print("TCA Port #"); Serial.println(t);
 
+      //Inner for loop - search for all possible i2c addresses
       for (uint8_t addr = 0; addr<=127; addr++) {
-        if (addr == TCA9548A) continue;
+        if (addr == TCA9548A) continue;   //Skip the TCA/breakout chip address
 
         Wire.beginTransmission(addr);
+        //If statement is true if something responded
         if (!Wire.endTransmission()) {
           Serial.print("Found I2C 0x");  Serial.println(addr,HEX);
         }
