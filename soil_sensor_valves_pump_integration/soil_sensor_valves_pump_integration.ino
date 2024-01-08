45f35e73aa1d5cb763c6c56d2d845dfe7d57191e
torrseba <113491671+torrseba@users.noreply.github.com>
2024-01-08 11:48:12 -0500

    minor code updates + starting Task1

--- soil_sensor_valves_pump_integration/soil_sensor_valves_pump_integration.ino
+++ soil_sensor_valves_pump_integration/soil_sensor_valves_pump_integration.ino
@@ -16,11 +16,23 @@ I2CHub mux; //object for the bus
 Pump pump; //creates object for the pump
 wifiConnect wifi; //wifConnect Object
 WiFiClient Client; //WifiClient for Thingspeak
+TaskHandle_t Task1; //Task for watering plants on second core
 
 
 
 void setup() {
   Serial.begin(115200);
+
+   //create a task that will be executed in the Task1code() function, with priority 1 and executed on core 0
+  xTaskCreatePinnedToCore(
+      Task1code,       /* Task function. */
+      "Task1",         /* name of task. */
+      10000,           /* Stack size of task */
+      NULL,            /* parameter of the task */
+      1,               /* priority of the task */
+      &Task1,          /* Task handle to keep track of created task */
+      0);              /* pin task to core 0 */
+    delay(500);
 
   wifi.initWifi();
   if (WiFi.status() == WL_CONNECT_FAILED) {
@@ -44,10 +56,16 @@ void setup() {
   //Serial.println("channels set up")
 }
 
-void loop() {
-  wifi.wifiCheck(); //TODO Add Wifi Error Message
+long lastThingspeakUpdateTime = 0;
 
-  //For ea
+void Task1code( void * pvParameters ){
+  Serial.print("Task1 running on core ");
+  Serial.println(xPortGetCoreID());
+
+}
+
+void loop() {
+  wifi.wifiCheck(); //TODO Add Wifi Error Message 
   for (uint8_t sampleNum = 0; sampleNum < SAMPLE_COUNT; sampleNum++){
     for (uint8_t i = 0; i < NUM_CHANNELS; i+=1) {
       Serial.printf("Channel: %d\n", i+1);
@@ -55,8 +73,41 @@ void loop() {
       channels[i].readSensor(sampleNum);
     }
     delay(THINGSPEAK_UPDATE_DELAY/SAMPLE_COUNT);
+  }//This for loop will take 15 seconds
+
+  if(millis() - lastThingspeakUpdateTime >= THINGSPEAK_UPDATE_DELAY){ //If 15 seconds have elapsed since last thingSpeak update
+    for(int i=0; i < NUM_CHANNELS; i++){ //TODO add 15 second delay to send data, separate valves so doesnt mess with packet sending.
+      float avgReading = channels[i].averageSamplesAndPublish(i);
+      if(avgReading < SENSOR_THRESHOLD) {
+
+
+
+      }
+    }
+    lastThingspeakUpdateTime = millis();
   }
   
+
+  //REFERENCE CODE TO ADD ERROR-CODES FOR UNSUCESSFUL PUSHES TO THINGSPEAK
+  /*//time for a new thingspeak update
+  if(millis() - lastThingspeakUpdateTime >= thingspeakUpdateDelay ){
+    // write to the ThingSpeak channel
+    averageSamplesAndPublish();
+    ThingSpeak.setStatus(myStatus);
+    
+    int x = ThingSpeak.writeFields(myChannelNumber, myWriteAPIKey);
+    if(x == 200){
+      Serial.println("Channel update successful.");
+      lastThingspeakUpdateTime = millis();
+    }else{
+      Serial.println("Problem updating channel. HTTP error code " + String(x));
+      myStatus += String("Problem updating channel. HTTP error code " + String(x));
+      delay(5000);
+    }//if else for thingspeak
+  }//if millis() delay for thingspeak*/
+
+  
+  /*
   for(int i=0; i < NUM_CHANNELS; i++){ //TODO add 15 second delay to send data, separate valves so doesnt mess with packet sending.
     float avgReading = channels[i].averageSamplesAndPublish(i);
     if(avgReading < SENSOR_THRESHOLD) {
@@ -72,6 +123,6 @@ void loop() {
     else{
       delay(1000);
     }
-  }
+  }*/
   
 }
